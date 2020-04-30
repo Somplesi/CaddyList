254c241c0a4bf444b3989df51dcef33c535bf818
Rodolphe DUPUY <rodolphe.dupuy@free.fr>
2020-04-30 08:32:24 +0200

    fix List

--- LifeGoals/ViewController.swift
+++ LifeGoals/ViewController.swift
@@ -19,18 +19,11 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
     
     @IBOutlet weak var outlet_TableView: UITableView!
     
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Add", message: "Please", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Add Item", style: .default, handler: .none)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //print("Appel du DidLoad") // Test
 
-        print("Appel du DidLoad") // Test
-        
         //  Delegations
         outlet_TextInput.delegate = self  // protocole UITextFieldDelegate
         outlet_TableView.delegate = self // protocole UITableViewDelegate
@@ -40,13 +33,30 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
         loadItems()
     }
 
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        //print("Edit button clic")
+        //            tableView.setEditing(true, animated: true)
+        //            tableView.allowsSelectionDuringEditing = true  // Test TableView Edit mode
+        if outlet_TableView.isEditing == true {
+            outlet_TableView.setEditing(false, animated: true)
+        } else {
+            outlet_TableView.setEditing(true, animated: true)
+        }
+    }
+    
+    @IBAction func clearButton(_ sender: UIBarButtonItem) {
+        for index in 0..<itemArray.count {
+                itemArray[index].highLighted = false
+                itemArray[index].done = false
+        }
+
+        saveItems()
+    }
     
     //MARK: - These TableView DataSource Methods are mandatories "required" by protocole of UITableViewDataSource (see Help)
     //  Set the internal table itemArray Row Count
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
-        print("Lignes dispo \(itemArray.count)") // Test
-           
+        //print("Lignes dispo \(itemArray.count)") // Test
         return itemArray.count
        }
     //  Fill the value into TableView from internal table ItemArray called for each line
@@ -54,50 +64,86 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
         
         let cell=tableView.dequeueReusableCell(withIdentifier: "uiCellTable", for: indexPath)
         let item = itemArray[indexPath.row]
-        
+        // Fill the text into TableView
         cell.textLabel?.text = item.itemLifeGoals
+        // Check or UnCheck
         cell.accessoryType = item.done ? .checkmark : .none
-
-        print("Ligne chargée: \(item.itemLifeGoals!) \(item.done)") // Test
-        
+        // HighLight or not
+        if item.highLighted == true {
+            //cell.backgroundColor = .blue
+            cell.textLabel?.isEnabled = true
+        } else {
+            //cell.backgroundColor = .green
+            cell.textLabel?.isEnabled = false
+        }
+        //print("Ligne chargée: \(item.itemLifeGoals!) \(item.done)") // Test
         return cell
     }
 
     //MARK: - Select a row - TableView Delegate Methods
         func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-            print("Select ligne: \(indexPath.row)") // Test
             
+            // itemArray[indexPath.row].done = !itemArray[indexPath.row].done  // Check or Uncheck line
+            if itemArray[indexPath.row].done == true && itemArray[indexPath.row].highLighted == true {
+                itemArray[indexPath.row].highLighted = false
+                itemArray[indexPath.row].done = false
+            } else {
+                if itemArray[indexPath.row].highLighted == true {
+                    itemArray[indexPath.row].done = true
+                } else {
+                    itemArray[indexPath.row].highLighted = true
+                    itemArray[indexPath.row].done = false
+                }
+            }
+
             // tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            itemArray[indexPath.row].done = !itemArray[indexPath.row].done  // Check or Uncheck line
             tableView.deselectRow(at: indexPath, animated: true)
             // tableView.selectRow(at: indexPath, animated: true, scrollPosition: UITableView.ScrollPosition.top)
             saveItems()
         }
-        
+    
+    
+    //MARK: - Move row
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // print("Row moved")
+    }
+    
+    //MARK: - TableView Editable
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.section == 0 ? true : false
+//        if indexPath.section == 1 {
+//            return true
+//        }
+//        return false
+    }
+    
+    //MARK: Row can move
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // print("Row can move")
+        return true
+    }
+    
     //MARK: - Delete management into TableView
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         context.delete(itemArray[indexPath.row])
         itemArray.remove(at: indexPath.row)
-
-        print("Delete de l'indexe: \(indexPath.row)") // Test
-        
+        //print("Delete de l'indexe: \(indexPath.row)") // Test
         tableView.deleteRows(at: [indexPath], with: .automatic) // with animation
-
         self.saveItems()
+        print("EditingStyle called")
+      
     }
 
     //MARK: - Add new Items
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        if let charactFill = outlet_TextInput.text?.count { // Check if Input line is filled
+        if let charactFill = outlet_TextInput.text?.count { // Check if Input line is filled (method 2)
             if charactFill > 0 {
                 let newItem = ToDoItem(context: self.context)
                 newItem.itemLifeGoals = outlet_TextInput.text!
                 newItem.done = false
+                newItem.highLighted = false
                 self.itemArray.append(newItem)
-                
-                print("Ajout Ligne \(outlet_TextInput.text!)") // Test
+                //print("Ajout Ligne \(outlet_TextInput.text!)") // Test
                 self.saveItems()
             }
         }
@@ -110,9 +156,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
     func loadItems(with request: NSFetchRequest<ToDoItem>=ToDoItem.fetchRequest()) {
         do { // into internal table itemArray from CoreData
             itemArray = try context.fetch(request)
-
-            print("Lecture CoreData & Chargement dans itemArray") // Test
-
+            //print("Lecture CoreData & Chargement dans itemArray") // Test
         } catch {
             print("Error fetching data from context \(error)")
         }
@@ -123,9 +167,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
     func saveItems() {
         do {
           try context.save()
-
-            print("Sauvegarde dans CoreData") // Test
-
+            //print("Sauvegarde dans CoreData") // Test
         } catch {
            print("Error saving context \(error)")
         }
@@ -138,4 +180,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
         textField.resignFirstResponder() // Keyboard off
         return false
     }
+    
 }
