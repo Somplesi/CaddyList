ed87f058db6d328a61fbbd0d1fb78fc52c3bac60
Rodolphe DUPUY <rodolphe.dupuy@free.fr>
2020-05-09 07:24:57 +0200

    Optimization

--- LifeGoals/ViewController.swift
+++ LifeGoals/ViewController.swift
@@ -10,29 +10,28 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
-
-    var itemArray = [ToDoItem]()    // Internal Table sync with CoreData
+    
+    var itemArray = [ToDoItem]()    // Internal Table sync with CoreData directly
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
+    
     @IBOutlet weak var outlet_TextInput: UITextField!
-    
     @IBOutlet weak var outlet_TableView: UITableView!
-    
+    @IBOutlet weak var displayChoose: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //print("Appel du DidLoad") // Test
-
+        
         //  Delegations
         outlet_TextInput.delegate = self  // protocole UITextFieldDelegate
         outlet_TableView.delegate = self // protocole UITableViewDelegate
         outlet_TableView.dataSource = self // protocole UITableViewDataSource
-
+        
         // CoreData READ
         loadItems()
     }
-
+    
     @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
         //print("Edit button clic")
         //            tableView.setEditing(true, animated: true)
@@ -46,94 +45,107 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
     
     @IBAction func clearButton(_ sender: UIBarButtonItem) {
         for index in 0..<itemArray.count {
-                itemArray[index].highLighted = false
-                itemArray[index].done = false
+            itemArray[index].highLighted = false
+            itemArray[index].done = false
         }
-
         saveItems()
     }
     
+    @IBAction func displayList(_ sender: UISegmentedControl) {
+        outlet_TableView.reloadData()
+    }
+    
     //MARK: - These TableView DataSource Methods are mandatories "required" by protocole of UITableViewDataSource (see Help)
-    //  Set the internal table itemArray Row Count
+    //  Set the internal table itemArray Row Count - Required by delegation
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //print("Lignes dispo \(itemArray.count)") // Test
         return itemArray.count
-       }
-    //  Fill the value into TableView from internal table ItemArray called for each line
+    }
+    //  Fill the value into TableView from internal table ItemArray called for each line - Required by delegation
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell=tableView.dequeueReusableCell(withIdentifier: "uiCellTable", for: indexPath)
         let item = itemArray[indexPath.row]
-        // Fill the text into TableView
-        cell.textLabel?.text = item.itemLifeGoals
-        // Check or UnCheck
-        cell.accessoryType = item.done ? .checkmark : .none
-        // HighLight or not
-        if item.highLighted == true {
-            //cell.backgroundColor = .blue
-            cell.textLabel?.isEnabled = true
-        } else {
-            //cell.backgroundColor = .green
-            cell.textLabel?.isEnabled = false
-        }
+        // Display List
+//        if displayChoose.selectedSegmentIndex == 0 {
+            // Fill the text into TableView
+            cell.textLabel?.text = item.itemLifeGoals
+            // Check or UnCheck
+            cell.accessoryType = item.done ? .checkmark : .none
+            // HighLight or not
+            cell.textLabel?.isEnabled = item.highLighted
+//            if item.highLighted == true {
+//                //cell.backgroundColor = .blue
+//                cell.textLabel?.isEnabled = true
+//            } else {
+//                //cell.backgroundColor = .green
+//                cell.textLabel?.isEnabled = false
+//            }
+//        }
+//        if displayChoose.selectedSegmentIndex == 1 && item.highLighted == true && item.done == false {
+//            cell.textLabel?.text = item.itemLifeGoals
+//            cell.accessoryType = .none
+//            cell.textLabel?.isEnabled = item.highLighted
+//        }
+//        if displayChoose.selectedSegmentIndex == 2 && item.highLighted == true && item.done == true {
+//            cell.textLabel?.text = item.itemLifeGoals
+//            cell.accessoryType = .checkmark
+//            cell.textLabel?.isEnabled = item.highLighted
+//        }
         //print("Ligne chargée: \(item.itemLifeGoals!) \(item.done)") // Test
         return cell
     }
-
+    
+    
+    //MARK: Refresh List when scroll down
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        outlet_TableView.reloadData()
+    }
+    
     //MARK: - Select a row - TableView Delegate Methods
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            
-            // itemArray[indexPath.row].done = !itemArray[indexPath.row].done  // Check or Uncheck line
-            if itemArray[indexPath.row].done == true && itemArray[indexPath.row].highLighted == true {
-                itemArray[indexPath.row].highLighted = false
-                itemArray[indexPath.row].done = false
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Check or Uncheck line
+        if itemArray[indexPath.row].done == true && itemArray[indexPath.row].highLighted == true {
+            itemArray[indexPath.row].highLighted = false
+            itemArray[indexPath.row].done = false
+        } else {
+            if itemArray[indexPath.row].highLighted == true {
+                itemArray[indexPath.row].done = true
             } else {
-                if itemArray[indexPath.row].highLighted == true {
-                    itemArray[indexPath.row].done = true
-                } else {
-                    itemArray[indexPath.row].highLighted = true
-                    itemArray[indexPath.row].done = false
-                }
+                itemArray[indexPath.row].highLighted = true
+                itemArray[indexPath.row].done = false
             }
-
-            // tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            tableView.deselectRow(at: indexPath, animated: true)
-            // tableView.selectRow(at: indexPath, animated: true, scrollPosition: UITableView.ScrollPosition.top)
-            saveItems()
         }
+        // tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        tableView.deselectRow(at: indexPath, animated: true)
+        // tableView.selectRow(at: indexPath, animated: true, scrollPosition: UITableView.ScrollPosition.top)
+        saveItems()
+    }
     
     
-    //MARK: - Move row
+    //MARK: - Row Moved
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        // print("Row moved")
+        
     }
     
     //MARK: - TableView Editable
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return indexPath.section == 0 ? true : false
-//        if indexPath.section == 1 {
-//            return true
-//        }
-//        return false
     }
     
     //MARK: Row can move
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // print("Row can move")
         return true
     }
     
     //MARK: - Delete management into TableView
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         context.delete(itemArray[indexPath.row])
-        itemArray.remove(at: indexPath.row)
-        //print("Delete de l'indexe: \(indexPath.row)") // Test
+        itemArray.remove(at: indexPath.row) // Delete line into array using index
         tableView.deleteRows(at: [indexPath], with: .automatic) // with animation
         self.saveItems()
-        print("EditingStyle called")
-      
     }
-
+    
     //MARK: - Add new Items
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if let charactFill = outlet_TextInput.text?.count { // Check if Input line is filled (method 2)
@@ -142,8 +154,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
                 newItem.itemLifeGoals = outlet_TextInput.text!
                 newItem.done = false
                 newItem.highLighted = false
-                self.itemArray.append(newItem)
-                //print("Ajout Ligne \(outlet_TextInput.text!)") // Test
+                self.itemArray.append(newItem)  // Add line into array & CoreData
                 self.saveItems()
             }
         }
@@ -154,9 +165,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
     
     //MARK: - Load data
     func loadItems(with request: NSFetchRequest<ToDoItem>=ToDoItem.fetchRequest()) {
+        context.automaticallyMergesChangesFromParent = true
         do { // into internal table itemArray from CoreData
-            itemArray = try context.fetch(request)
-            //print("Lecture CoreData & Chargement dans itemArray") // Test
+            itemArray = try context.fetch(request)  // Read CoreData & Load into array
         } catch {
             print("Error fetching data from context \(error)")
         }
@@ -165,11 +176,11 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDataSour
     
     //MARK: - Save data
     func saveItems() {
+        context.automaticallyMergesChangesFromParent = true
         do {
-          try context.save()
-            //print("Sauvegarde dans CoreData") // Test
+            try context.save() // Save into CoreData
         } catch {
-           print("Error saving context \(error)")
+            print("Error saving context \(error)")
         }
         self.outlet_TableView.reloadData() // Refresh TableView
     }
